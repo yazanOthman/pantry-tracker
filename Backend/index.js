2ba0304dab45aa7bff7df587282af2e63a669eb2
Yazan Othman <yazanothman@Yazans-Laptop.local>
2024-08-08 09:52:38 +0800

    updated cors origin

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://pantry-tracker-beta-three.vercel.app",
     credentials: true,
   })
 );
